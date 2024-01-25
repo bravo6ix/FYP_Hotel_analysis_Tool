03d50499825155d9731df20cb3899fb0fbca212d
ming <qq15815732077@gmail.com>
2024-01-25 13:12:13 +0800

    install npm bcrypt package

--- hotel-dashboard_backend/routes/users.js
+++ hotel-dashboard_backend/routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* token */
+let jwt = require("jsonwebtoken");
+let auth = require("../middlewares/auth");
+const { v4: uuidv4 } = require("uuid");
+
+/* bcrypt */
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+/* MongoDB Client */
+const { MongoClient } = require("mongodb");
+let ObjectId = require("mongodb").ObjectId;
+const url =
+    "mongodb://groupd:vlQz3qq10nGdmFSb@ac-gm2iezg-shard-00-00.eyyijsu.mongodb.net:27017,ac-gm2iezg-shard-00-01.eyyijsu.mongodb.net:27017,ac-gm2iezg-shard-00-02.eyyijsu.mongodb.net:27017/?ssl=true&replicaSet=atlas-wzwa6h-shard-0&authSource=admin&retryWrites=true&w=majority";
+const client = new MongoClient(url);
+const db = client.db("cubcGroupD-DB");
 
 module.exports = router;
