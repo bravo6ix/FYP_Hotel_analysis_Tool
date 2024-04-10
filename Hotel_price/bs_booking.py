821346cba13d2ec1afe678b4070b22e5583762c9
Ming <qq15815732077@gmail.com>
2024-04-10 22:44:27 +0800

    update process

--- Hotel_price/bs_booking.py
+++ Hotel_price/bs_booking.py
@@ -6,16 +6,16 @@ import pandas as pd
 import re
 import time
 
-base_url = 'https://www.booking.com/searchresults.en-gb.html?ss=Hong+Kong&ssne=Hong+Kong&ssne_untouched=Hong+Kong&label=gog235jc-1DCAEoggI46AdIM1gDaGKIAQGYAQm4AQfIAQzYAQPoAQGIAgGoAgO4ApiJ-6UGwAIB0gIkYzI2N2E0ZjAtYmFhOS00YjUzLTliYTUtZjk1NDBlYmMzN2Fj2AIE4AIB&sid=bc9a4cbfda9728a8ab892f66ceaa1a79&aid=397617&lang=en-gb&sb=1&src_elem=sb&src=searchresults&dest_id=-1353149&dest_type=city&checkin=2024-01-05&checkout=2024-01-06&ltfd=6%3A2%3A1-2024_2-2024_3-2024%3A1%3A1&group_adults=2&no_rooms=1&group_children=0'
+base_url = 'https://www.booking.com/searchresults.en-gb.html?ss=Hong+Kong&ssne=Hong+Kong&ssne_untouched=Hong+Kong&efdco=1&label=gog235jc-1BCAEoggI46AdIM1gDaGKIAQGYAQm4AQfIAQzYAQHoAQGIAgGoAgO4ApiJ-6UGwAIB0gIkYzI2N2E0ZjAtYmFhOS00YjUzLTliYTUtZjk1NDBlYmMzN2Fj2AIF4AIB&sid=bc9a4cbfda9728a8ab892f66ceaa1a79&aid=397617&lang=en-gb&sb=1&src_elem=sb&src=searchresults&dest_id=-1353149&dest_type=city&checkin=2024-05-01&checkout=2024-05-04&ltfd=6%3A2%3A%3A%3A1&group_adults=2&no_rooms=1&group_children=0'
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
     'Accept-Language': 'en-US, en;q=0.5'
 }
 
 response = requests.get(base_url, headers=headers)
-time.sleep(5)
-soup = BeautifulSoup(response.text, 'html.parser')
-
+#time.sleep(5)
+# soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response.text, "lxml")
 
 # Find all the hotel elements in the HTML document
 hotels = soup.findAll('div', {'data-testid': 'property-card'})
@@ -47,6 +47,7 @@ def scrape_hotel_data(hotel):
     else:
         price = 0
 
+
     # Extract the hotel rating
     rating_element = hotel.find('div', {'class': 'a3b8729ab1 d86cee9b25'})
     if rating_element is not None:
@@ -58,13 +59,71 @@ def scrape_hotel_data(hotel):
     # Time for scraping
     month2 = datetime.now().strftime('%b')
 
+    # Extract the target start and end dates
+    startweek_element = hotel.find('div', {'class': 'a8887b152e'})
+    if startweek_element is not None:
+        startweek = startweek_element.text.strip()
+        startweek = startweek[:3]
+    else:
+        startweek = 'N/A'
+
+    startday_element = hotel.find('div', {'class': 'a8887b152e'})
+    if startday_element is not None:
+        start_month_day = startday_element.text.strip()
+        start_month_day = ''.join(re.findall(r'\d+', start_month_day))
+    else:
+        start_month_day = 'N/A'
+
+    startmonth_element = hotel.find('div', {'class': 'a8887b152e'})
+    if startmonth_element is not None:
+        start_month = startmonth_element.text.strip()
+        start_month = start_month[-3:]
+    else:
+        start_month = 'N/A'
+
+    # End
+    endweek_element = hotel.find('div', {'class': 'a8887b152e'})
+    if endweek_element is not None:
+        end_week = endweek_element.text.strip()
+        end_week = end_week[:3]
+    else:
+        end_week = 'N/A'
+
+    endday_element = hotel.find('div', {'class': 'a8887b152e'})
+    if endday_element is not None:
+        end_month_day = endday_element.text.strip()
+        end_month_day = ''.join(re.findall(r'\d+', end_month_day))
+    else:
+        end_month_day = 'N/A'
+
+    endmonth_element = hotel.find('div', {'class': 'a8887b152e'})
+    if endmonth_element is not None:
+        end_month = endmonth_element.text.strip()
+    else:
+        end_month = 'N/A'
+
+    # Extract the number of views
+    platform_views_element = hotel.find('div', {'class': 'abf093bdfe f45d8e4c32 d935416c47'})
+    if platform_views_element is not None:
+        platform_views = platform_views_element.text.strip()
+        platform_views = int(''.join(re.findall(r'\d+', platform_views.replace(',', ''))))
+    else:
+        platform_views = 0
+
     # Append hotes_data with info about hotel
     hotel_data = {
         'hotel_name': name,
         'district': location,
         'price': price,
         'rating': ratings,
-        'month': month2,
+        'scraped_month': month2,
+        'views': platform_views,
+        'target_start_week' : startweek,
+        'target_start_month': start_month,
+        'target_start_month_day': start_month_day,
+        'target_end_week' : end_week,
+        'target_end_month': end_month,
+        'target_end_month_day': end_month_day,
     }
 
     return hotel_data
@@ -76,7 +135,7 @@ def scrape_all_hotels(base_url):
     while offset <= 100:
         url = f"{base_url}?offset={offset}"
         response = requests.get(url, headers=headers)
-        time.sleep(5)
+        time.sleep(2)
         soup = BeautifulSoup(response.text, 'html.parser')
         hotels = soup.findAll('div', {'data-testid': 'property-card'})
 
@@ -100,5 +159,7 @@ collection = db['test_ingestion']
 data_dict = hotelss.to_dict("records")
 collection.insert_many(data_dict)
 
-hotelss.to_csv('Hotel_price/bs_hotels.csv', header=True, index=False)
-hotelss.to_json('Hotel_price/bs_hotels.json', orient='records')
+# Current time
+current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+hotelss.to_csv(f'Hotel_price/sraped_folder/{current_time}_bs_hotels.csv', header=True, index=False)
+hotelss.to_json(f'Hotel_price/sraped_folder/{current_time}_bs_hotels.json', orient='records')
--- Hotel_price/qt.py
+++ Hotel_price/qt.py
@@ -1,71 +1,182 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLineEdit
-from bs4 import BeautifulSoup
-import requests
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLineEdit, QLabel
+from PyQt5.QtCore import Qt
 from pymongo import MongoClient
+from bs4 import BeautifulSoup
 from datetime import datetime
-import json
+from pymongo import MongoClient
+import requests
+import pandas as pd
+import re
+import time
 
-class ScraperApp(QWidget):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.initUI()
+        self.mongo_label = QLabel("MongoDB connect string")
+        self.mongo_input = QLineEdit()
 
-    def initUI(self):
-        self.layout = QVBoxLayout()
+        self.address_label = QLabel("Booking target address")
+        self.address_input = QLineEdit()
 
-        self.url_input = QLineEdit()
-        self.layout.addWidget(self.url_input)
+        self.collection_label = QLabel("MongoDB collection")
+        self.collection_input = QLineEdit()
 
-        self.start_button = QPushButton('Start Scrapy')
-        self.start_button.clicked.connect(self.start_scrapy)
-        self.layout.addWidget(self.start_button)
+        self.scrape_button = QPushButton("Scrape")
+        self.scrape_button.clicked.connect(self.scrape)
 
-        self.setLayout(self.layout)
+        layout = QVBoxLayout()
+        layout.addWidget(self.mongo_label)
+        layout.addWidget(self.mongo_input)
+        layout.addWidget(self.address_label)
+        layout.addWidget(self.address_input)
+        layout.addWidget(self.collection_label)
+        layout.addWidget(self.collection_input)
+        layout.addWidget(self.scrape_button)
 
-    def start_scrapy(self):
-        base_url = "https://www.booking.com/searchresults.en-gb.html?label=gog235jc-1DCAEoggI46AdIM1gDaGKIAQGYAQm4AQfIAQzYAQPoAQGIAgGoAgO4ApiJ-6UGwAIB0gIkYzI2N2E0ZjAtYmFhOS00YjUzLTliYTUtZjk1NDBlYmMzN2Fj2AIE4AIB&sid=bc9a4cbfda9728a8ab892f66ceaa1a79&aid=397617&ss=Hong+Kong&ssne=Hong+Kong&ssne_untouched=Hong+Kong&lang=en-gb&sb=1&src_elem=sb&src=searchresults&dest_id=-1353149&dest_type=city&checkin=2024-01-05&checkout=2024-01-06&ltfd=6%3A2%3A1-2024_2-2024_3-2024%3A1%3A1&group_adults=2&no_rooms=1&group_children=0"
-        data = []
+        self.setLayout(layout)
 
-        for offset in range(0, 501, 25):
-            url = f"{base_url}&offset={offset}"
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, "html.parser")
+    def scrape(self):
+        mongo_connect_string = self.mongo_input.text()
+        target_address = self.address_input.text()
+        collection_name = self.collection_input.text()
 
-            hotels = soup.find_all('div', {'data-testid': 'property-card'})
-
-            for hotel in hotels:
-                name = hotel.find('div', {'data-testid': 'title'}).text.strip()
-                location = hotel.find('span', {'data-testid': 'address'}).text.strip()
-                price = float(hotel.find('span', {'data-testid': 'price-and-discounted-price'}).text.strip().replace(u'\xa0', u' '))
-                rating = float(hotel.find('div', {'class': 'a3b8729ab1 d86cee9b25'}).text.strip())
-                time = datetime.now().strftime('%Y-%m')
-                month = datetime.now().strftime('%b')
-                district = location.split(',')[1] if ',' in location else location
-
-                data.append({
-                    'hotel_name': name,
-                    'location': location,
-                    'price': price,
-                    'rating': rating,
-                    'time': time,
-                    'month': month,
-                    'district': district
-                })
-
-        client = MongoClient('mongodb+srv://qq1027784227:qq1027784227@hotel.0a4is7r.mongodb.net/')
+        client = MongoClient(mongo_connect_string)
         db = client['hotels']
-        collection = db['booking_price']
-        collection.insert_many(data)
+        collection = db[collection_name]
 
-        print(f'Successfully scraped and saved data from {base_url}')
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'
+        }
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
+        response = requests.get(target_address, headers=headers)
+        soup = BeautifulSoup(response.text, "lxml")
 
-    ex = ScraperApp()
+        hotels = soup.findAll('div', {'data-testid': 'property-card'})
 
-    ex.show()
+        hotels_data = []
+        for hotel in hotels:
+            # Here you can call your scrape function
+            # hotel_data = scrape_hotel_data(hotel)
+            # hotels_data.append(hotel_data)
+            #Extract the hotel name
+            name_element = hotel.find('div', {'data-testid': 'title'})
+            if name_element is not None:
+                name = name_element.text.strip()
+            else:
+                name = 'N/A'
 
-    sys.exit(app.exec_())
+            # Extract the hotel location
+            location_element = hotel.find('span', {'data-testid': 'address'})
+            if location_element is not None:
+                location = location_element.text.strip().split(',')[0]
+                location = location.split(' District')[0]
+            else:
+                location = 'N/A'
+
+
+            # Extract the hotel price
+            price_element = hotel.find('span', {'data-testid': 'price-and-discounted-price'})
+            if price_element is not None:
+                price = price_element.text.strip().replace(u'\xa0', u' ')
+                price = int(''.join(re.findall(r'\d+', price.replace(',', ''))))
+            else:
+                price = 0
+
+
+            # Extract the hotel rating
+            rating_element = hotel.find('div', {'class': 'a3b8729ab1 d86cee9b25'})
+            if rating_element is not None:
+                rating = rating_element.text.strip()
+                ratings = float(rating.split('Scored')[0].strip())
+            else:
+                ratings = 0
+
+            # Time for scraping
+            month2 = datetime.now().strftime('%b')
+
+            # Extract the target start and end dates
+            startweek_element = hotel.find('div', {'class': 'a8887b152e'})
+            if startweek_element is not None:
+                startweek = startweek_element.text.strip()
+                startweek = startweek[:3]
+            else:
+                startweek = 'N/A'
+
+            startday_element = hotel.find('div', {'class': 'a8887b152e'})
+            if startday_element is not None:
+                start_month_day = startday_element.text.strip()
+                start_month_day = ''.join(re.findall(r'\d+', start_month_day))
+            else:
+                start_month_day = 'N/A'
+
+            startmonth_element = hotel.find('div', {'class': 'a8887b152e'})
+            if startmonth_element is not None:
+                start_month = startmonth_element.text.strip()
+                start_month = start_month[-3:]
+            else:
+                start_month = 'N/A'
+
+            # End
+            endweek_element = hotel.find('div', {'class': 'a8887b152e'})
+            if endweek_element is not None:
+                end_week = endweek_element.text.strip()
+                end_week = end_week[:3]
+            else:
+                end_week = 'N/A'
+
+            endday_element = hotel.find('div', {'class': 'a8887b152e'})
+            if endday_element is not None:
+                end_month_day = endday_element.text.strip()
+                end_month_day = ''.join(re.findall(r'\d+', end_month_day))
+            else:
+                end_month_day = 'N/A'
+
+            endmonth_element = hotel.find('div', {'class': 'a8887b152e'})
+            if endmonth_element is not None:
+                end_month = endmonth_element.text.strip()
+            else:
+                end_month = 'N/A'
+
+            # Extract the number of views
+            platform_views_element = hotel.find('div', {'class': 'abf093bdfe f45d8e4c32 d935416c47'})
+            if platform_views_element is not None:
+                platform_views = platform_views_element.text.strip()
+                platform_views = int(''.join(re.findall(r'\d+', platform_views.replace(',', ''))))
+            else:
+                platform_views = 0
+
+            # Append hotes_data with info about hotel
+            hotel_data = {
+                'hotel_name': name,
+                'district': location,
+                'price': price,
+                'rating': ratings,
+                'scraped_month': month2,
+                'views': platform_views,
+                'target_start_week' : startweek,
+                'target_start_month': start_month,
+                'target_start_month_day': start_month_day,
+                'target_end_week' : end_week,
+                'target_end_month': end_month,
+                'target_end_month_day': end_month_day,
+            }
+
+            return hotel_data
+        all_hotels_data = scrape_all_hotels(address_input)
+        hotels = pd.DataFrame(all_hotels_data)
+        hotelss = hotels.drop_duplicates()
+        data_dict = hotelss.to_dict("records")
+        collection.insert_many(hotels_data)
+
+    # Current time
+    current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    hotelss.to_csv(f'Hotel_price/sraped_folder/{current_time}_bs_hotels.csv', header=True, index=False)
+    hotelss.to_json(f'Hotel_price/sraped_folder/{current_time}_bs_hotels.json', orient='records')
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec_()
